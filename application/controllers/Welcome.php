09234c1c7e62dc99e4bbec64b7cd67b7449b01a3
John Janzen <joalja2002@msn.com>
2016-03-21 15:02:51 -0700

    Welcome View - John

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,10 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('welcome_message');
+                $this->data['TableContent'] = "this";
+                $this->data['title'] = "TimeTable";
+                $this->data['data'] = $this->data;
+                $this->parser->parse('Welcome', $this->data);
 	}
         
         public function search()
